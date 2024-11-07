a2f759956fa84c31e7aada418107b66f37a6b22e
JinHyung-Park <trupjh@gmail.com>
2024-11-07 21:53:05 +0900

    feat(OutputView) : 상품 정보 출력을 위만 메소드 작성

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -1,8 +1,15 @@
 package store.view;
 
+import java.util.List;
+import store.domain.Product;
+
 public class OutputView {
     public void helloCustomer() {
         System.out.println("안녕하세요. W편의점입니다.");
-        System.out.println("현재 보유하고 있는 상품입니다.");
+        System.out.println("현재 보유하고 있는 상품입니다.\n");
+    }
+
+    public void printProduct(String product) {
+        System.out.println("- " + product);
     }
 }
