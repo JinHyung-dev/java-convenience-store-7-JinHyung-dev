088dd59107d1bbdaf38241da916835df3ae0cdbc
JinHyung-Park <trupjh@gmail.com>
2024-11-08 22:37:43 +0900

    fix(ProductTest) : 상품 출력 toString 테스트를 위한 로직 수정
    - 수정된 Product 생성자 적용

--- src/test/java/store/domain/ProductTest.java
+++ src/test/java/store/domain/ProductTest.java
@@ -6,8 +6,9 @@ import org.junit.jupiter.api.Test;
 
 public class ProductTest {
     @Test
-    void 상품출력() {
-        assertThat(new Product("콜라", 1000, 10, "탄산2+1").toString())
+    void 프로모션_상품출력() {
+        Promotion promotion = new Promotion("탄산2+1","2","1","2024-01-01","2024-12-31");
+        assertThat(new Product("콜라", 1000, 10, promotion).toString())
                 .contains("탄산");
     }
 }
