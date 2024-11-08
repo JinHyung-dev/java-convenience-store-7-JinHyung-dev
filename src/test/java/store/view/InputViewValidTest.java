21011f82554e2bfd6633d3b4b6e91bc6b04e9a55
JinHyung-Park <trupjh@gmail.com>
2024-11-09 00:29:55 +0900

    test(InputViewValidTest) : 구매 입력 형식 유효성 테스트, 에러 처리 테스트 작성 및 완료

---
+++ src/test/java/store/view/InputViewValidTest.java
@@ -0,0 +1,19 @@
+package store.view;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class InputViewValidTest {
+    @Test
+    void 입력형식_테스트() {
+        InputView inputView = new InputView();
+        assertThrows(IllegalArgumentException.class, () -> {
+            inputView.validate("[tk");  // 유효하지 않은 입력값
+        });
+
+        assertTrue(inputView.invalidFormat("[사이다-2]")); //유효 입력
+        assertTrue(inputView.invalidFormat("[사이다-2], [콜라-1]")); //유효 다중 입력
+    }
+}
