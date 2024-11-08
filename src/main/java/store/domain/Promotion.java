3989be068978da00ec9137a014fe5c781c86b164
JinHyung-Park <trupjh@gmail.com>
2024-11-08 17:58:18 +0900

    feat(Promotion) : 프로모션 정보를 관리할 도메인 클래스 생성

---
+++ src/main/java/store/domain/Promotion.java
@@ -0,0 +1,37 @@
+package store.domain;
+
+public class Promotion {
+   private final String name;
+   private final int buy;
+   private final int get;
+   private final String startDate;
+   private final String endDate;
+
+    public Promotion(String name, String buy, String get, String startDate, String endDate) {
+        this.name = name;
+        this.buy = Integer.parseInt(buy);
+        this.get = Integer.parseInt(get);
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBuy() {
+        return buy;
+    }
+
+    public int getGet() {
+        return get;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+}
