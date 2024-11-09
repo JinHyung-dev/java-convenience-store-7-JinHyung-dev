c059502bbbbd7d7eaf85c8266feaf307d3148e54
JinHyung-Park <trupjh@gmail.com>
2024-11-09 18:17:19 +0900

    test(PurchaseServiceTest) : 제품명-수량 파싱 인덱스 로직 테스트 작성 및 완료

--- src/test/java/store/service/PurchaseServiceTest.java
+++ src/test/java/store/service/PurchaseServiceTest.java
@@ -5,9 +5,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.io.ByteArrayInputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 
 public class PurchaseServiceTest {
+    static Stream<Object[]> provideTestData() {
+        return Stream.of(
+                new Object[] {"[컵라면-2]", createExpectedMap("컵라면", 2)},
+                new Object[] {"[커피-3],[물-2]", createExpectedMap("커피", 3, "물", 2)},
+                new Object[] {"[라면-1],[김-5]", createExpectedMap("라면", 1, "김", 5)},
+                new Object[] {"[사이다-2],[감자칩-1],[콜라-3]", createExpectedMap("사이다", 2, "감자칩", 1, "콜라", 3)}
+        );
+    }
+
+    private static Map<String, Integer> createExpectedMap(Object... keysAndValues) {
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < keysAndValues.length; i += 2) {
+            map.put((String) keysAndValues[i], (Integer) keysAndValues[i + 1]);
+        }
+        return map;
+    }
+
     @Test
     void 파싱테스트() {
         PurchaseService service = new PurchaseService();
