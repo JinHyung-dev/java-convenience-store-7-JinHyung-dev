99c521b8438daf5213b136c71420edf123773547
JinHyung-Park <trupjh@gmail.com>
2024-11-09 21:23:45 +0900

    refactor(PurchaseServiceTest) : 공통 System.setIn세팅 내용 메서드로 분리

--- src/test/java/store/service/PurchaseServiceTest.java
+++ src/test/java/store/service/PurchaseServiceTest.java
@@ -5,25 +5,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.io.ByteArrayInputStream;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 
 public class PurchaseServiceTest {
-    static Stream<Object[]> provideTestData() {
-        return Stream.of(
-                new Object[] {"[컵라면-2]", createExpectedMap("컵라면", 2)},
-                new Object[] {"[커피-3],[물-2]", createExpectedMap("커피", 3, "물", 2)},
-                new Object[] {"[라면-1],[김-5]", createExpectedMap("라면", 1, "김", 5)},
-                new Object[] {"[사이다-2],[감자칩-1],[콜라-3]", createExpectedMap("사이다", 2, "감자칩", 1, "콜라", 3)}
-        );
-    }
-
-    private static Map<String, Integer> createExpectedMap(Object... keysAndValues) {
-        Map<String, Integer> map = new HashMap<>();
-        for (int i = 0; i < keysAndValues.length; i += 2) {
-            map.put((String) keysAndValues[i], (Integer) keysAndValues[i + 1]);
-        }
-        return map;
+    private void setSystemIn(String input) {
+        byte[] buf = input.getBytes();
+        System.setIn(new ByteArrayInputStream(buf));
     }
 
     @Test
@@ -33,8 +20,30 @@ public class PurchaseServiceTest {
         expect.put("컵라면", 2);
         expect.put("물", 1);
 
-        byte[] buf = "[컵라면-2],[물-1]".getBytes();
-        System.setIn(new ByteArrayInputStream(buf));
+        setSystemIn("[컵라면-2],[물-1]");
+
+        assertThat(service.getItems()).containsAllEntriesOf(expect);
+    }
+
+    @Test
+    void 파싱테스트2() {
+        PurchaseService service = new PurchaseService();
+        Map<String, Integer> expect = new HashMap<>();
+        expect.put("컵라면", 2);
+
+        setSystemIn("[컵라면-2]");
+
+        assertThat(service.getItems()).containsAllEntriesOf(expect);
+    }
+
+    @Test
+    void 파싱테스트3() {
+        PurchaseService service = new PurchaseService();
+        Map<String, Integer> expect = new HashMap<>();
+        expect.put("컵라면", 2);
+        expect.put("물", 1);
+
+        setSystemIn("[컵라면-2], [물-1]");
 
         assertThat(service.getItems()).containsAllEntriesOf(expect);
     }
