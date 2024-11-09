2f5094f95dd9f861f047d80341a5d35d5edea7f5
JinHyung-Park <trupjh@gmail.com>
2024-11-09 18:00:02 +0900

    feat(InputView) : 구매 입력 null 검증, trim 추가

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -3,15 +3,15 @@ package store.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    private final String ITEM_REGEX = "\\[(가-힣a-zA-Z)+-(\\d+)\\]";
+    private final String ITEM_REGEX = "^\\[[가-힣a-zA-Z]+-\\d+\\]$";
 
-    public void readItem() {
+    public String readItem() {
         while(true) {
             System.out.println("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
             try {
                 String input = Console.readLine();
                 validate(input);
-                break;
+                return input.trim();
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
@@ -19,12 +19,22 @@ public class InputView {
     }
 
     public void validate(String input) throws IllegalArgumentException{
-        if(invalidFormat(input)) {
-            throw new IllegalArgumentException("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
+        if(isNull(input)) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.");
+        }
+        String[] items = input.split(",");
+        for(String item : items) {
+            if(invalidFormat(item)) {
+                throw new IllegalArgumentException("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
+            }
         }
     }
 
     public boolean invalidFormat(String input) {
         return !input.matches(ITEM_REGEX);
+    }
+
+    private boolean isNull(String input) {
+        return input == null || input.trim().isEmpty();
     }
 }
