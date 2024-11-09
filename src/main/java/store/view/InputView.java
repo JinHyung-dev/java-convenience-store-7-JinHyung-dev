d6e7a2bd834d80f8bf79a43cad92019074d6b39f
JinHyung-Park <trupjh@gmail.com>
2024-11-10 00:17:10 +0900

    feat(InputView) : YN선택 입력 및 검증 로직 작성

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -4,13 +4,14 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private final String ITEM_REGEX = "^\\[[가-힣a-zA-Z]+-\\d+\\]$";
+    private final String CHOICE_REGEX = "^[YyNn]$";
 
     public String readItem() {
         while(true) {
             System.out.println("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
             try {
                 String input = Console.readLine();
-                validate(input);
+                validateItemQuantity(input);
                 return input.trim();
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
@@ -18,20 +19,39 @@ public class InputView {
         }
     }
 
-    public void validate(String input) throws IllegalArgumentException{
+    public void validateItemQuantity(String input) throws IllegalArgumentException{
         if(isNull(input)) {
             throw new IllegalArgumentException("[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.");
         }
         String[] items = input.split(",");
         for(String item : items) {
-            if(invalidFormat(item.trim())) {
+            if(invalidItemQuantityFormat(item.trim())) {
                 throw new IllegalArgumentException("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
             }
         }
     }
 
-    public boolean invalidFormat(String input) {
+    public boolean invalidItemQuantityFormat(String input) {
         return !input.matches(ITEM_REGEX);
+    }
+
+    public String readChoice() {
+        String input = Console.readLine().toUpperCase();
+        validateChoice(input);
+        return input.trim();
+    }
+
+    private void validateChoice(String input) {
+        if(isNull(input)) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.");
+        }
+        if(invalidChoice(input.trim())) {
+            throw new IllegalArgumentException("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private boolean invalidChoice(String input) {
+        return !input.matches(CHOICE_REGEX);
     }
 
     private boolean isNull(String input) {
