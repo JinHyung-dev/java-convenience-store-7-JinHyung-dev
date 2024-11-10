b4c841ea38dc482adea9ad395bb0204ed64d5085
JinHyung-Park <trupjh@gmail.com>
2024-11-11 01:50:11 +0900

    test(PurchaseServiceTest) : 기존 파싱 테스트 정상 수행 확인, 매장 없는 물건 입력시 에러 발생 테스트 작성 및 완료

--- src/test/java/store/service/PurchaseServiceTest.java
+++ src/test/java/store/service/PurchaseServiceTest.java
@@ -1,50 +1,33 @@
 package store.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.io.ByteArrayInputStream;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import org.junit.jupiter.api.Test;
 
 public class PurchaseServiceTest {
-    private void setSystemIn(String input) {
-        byte[] buf = input.getBytes();
-        System.setIn(new ByteArrayInputStream(buf));
+
+    @Test
+    void 파싱테스트_정상수행() {
+        PurchaseService service = new PurchaseService();
+        InventoryService.getInstance();
+        InventoryService.loadInventory(); //products 생성
+
+        assertThat(service.parseQuantity("[컵라면-2]")).isEqualTo(new String[]{"컵라면", "2"});
     }
 
     @Test
-    void 파싱테스트() {
-        PurchaseService service = new PurchaseService();
-        Map<String, Integer> expect = new HashMap<>();
-        expect.put("컵라면", 2);
-        expect.put("물", 1);
+    void 매장에_없는_물건_입력() {
+        InventoryService.getInstance();
+        InventoryService.loadInventory(); //products 생성
+        PurchaseService purchaseService = new PurchaseService();
+        OutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
 
-        setSystemIn("[컵라면-2],[물-1]");
-
-        assertThat(service.getItems()).containsAllEntriesOf(expect);
-    }
-
-    @Test
-    void 파싱테스트2() {
-        PurchaseService service = new PurchaseService();
-        Map<String, Integer> expect = new HashMap<>();
-        expect.put("컵라면", 2);
-
-        setSystemIn("[컵라면-2]");
-
-        assertThat(service.getItems()).containsAllEntriesOf(expect);
-    }
-
-    @Test
-    void 파싱테스트3() {
-        PurchaseService service = new PurchaseService();
-        Map<String, Integer> expect = new HashMap<>();
-        expect.put("컵라면", 2);
-        expect.put("물", 1);
-
-        setSystemIn("[컵라면-2], [물-1]");
-
-        assertThat(service.getItems()).containsAllEntriesOf(expect);
+        assertThrows(IllegalArgumentException.class,
+                () -> purchaseService.makeShoppingCart(new String[]{"[그냥워터-2]"}));
     }
 }
