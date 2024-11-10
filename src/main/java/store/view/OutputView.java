f57bb132e8fff1795a87f1dc52e49f60aaba00b7
JinHyung-Park <trupjh@gmail.com>
2024-11-11 03:33:49 +0900

    feat(OutputView) : 카트 상품 프로모션 증정 및 프로모션 불가 선택문 출력 메서드 추가

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -10,7 +10,11 @@ public class OutputView {
         System.out.println("- " + product);
     }
 
-    public void noticePromotionIsNotAvailable(String name, int quantity) {
+    public void printChoiceForFreeGet(String productName) {
+        System.out.println("현재 " + productName +" 은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)");
+    }
+
+    public void printChoiceForNoPromotion(String name, int quantity) {
         System.out.println("현재 " + name + " " + quantity + "개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)");
     }
 }
