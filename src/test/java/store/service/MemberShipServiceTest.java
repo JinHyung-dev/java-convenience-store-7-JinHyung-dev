5d1211479c701c5d4d0cbcf765224c4ea9446195
JinHyung-Park <trupjh@gmail.com>
2024-11-11 19:09:34 +0900

    test(MemberShipServiceTest) : 멤버십 적용 Y 선택시 할인율 적용 테스트 작성 및 완료

--- src/test/java/store/service/MemberShipServiceTest.java
+++ src/test/java/store/service/MemberShipServiceTest.java
@@ -6,14 +6,32 @@ import java.io.ByteArrayInputStream;
 import org.junit.jupiter.api.Test;
 
 public class MemberShipServiceTest {
-    private final Integer totalAmount = 12000;
+    private final Integer normalTotalAmount = 12000;
+    private final Integer manyTotalAmount = 50000;
+    private final Integer basicDiscountAmount = 8400;
+    private final Integer maxDiscountAmount = 42000;
 
     @Test
     void 기본30퍼할인() {
         MemberShipService memberShipService = new MemberShipService();
-        Integer sum = 8400;
         String input = "Y";
         System.setIn(new ByteArrayInputStream(input.getBytes()));
-        assertThat(memberShipService.getDiscountAmount(totalAmount)).isEqualTo(sum);
+        assertThat(memberShipService.getDiscountAmount(normalTotalAmount)).isEqualTo(basicDiscountAmount);
+    }
+
+    @Test
+    void 할인거부() {
+        MemberShipService memberShipService = new MemberShipService();
+        String input = "N";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        assertThat(memberShipService.getDiscountAmount(normalTotalAmount)).isEqualTo(normalTotalAmount);
+    }
+
+    @Test
+    void 최대할인초과불가() {
+        MemberShipService memberShipService = new MemberShipService();
+        String input = "Y";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        assertThat(memberShipService.getDiscountAmount(manyTotalAmount)).isEqualTo(maxDiscountAmount);
     }
 }
