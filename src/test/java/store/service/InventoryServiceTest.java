bf5a37385fa2bcd013d473902f852175c035efb2
JinHyung-Park <trupjh@gmail.com>
2024-11-11 14:29:20 +0900

    refactor(InventoryServiceTest) : 각 Product별 일반/프로모션 재고출력 테스트 내용 일부 변경 적용

--- src/test/java/store/service/InventoryServiceTest.java
+++ src/test/java/store/service/InventoryServiceTest.java
@@ -1,9 +1,12 @@
 package store.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.ByteArrayInputStream;
-import org.junit.jupiter.api.BeforeEach;
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import org.junit.jupiter.api.Test;
 import store.domain.Cart;
 
@@ -11,10 +14,35 @@ public class InventoryServiceTest {
     private InventoryService inventoryService;
     private PurchaseService purchaseService;
 
-    @BeforeEach
-    void 재고목록생성_및_카트생성() {
-        inventoryService = InventoryService.getInstance();
-        InventoryService.loadInventory(); //products 생성
+    @Test
+    void 재고출력() {
+        OutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        InventoryService inventoryService = InventoryService.getInstance();
+        inventoryService.printStock();
+
+        assertThat(outputStream.toString())
+                .contains(
+                "- 콜라 1,000원 10개 탄산2+1",
+                "- 콜라 1,000원 10개",
+                "- 사이다 1,000원 8개 탄산2+1",
+                "- 사이다 1,000원 7개",
+                "- 오렌지주스 1,800원 9개 MD추천상품",
+                "- 오렌지주스 1,800원 재고 없음",
+                "- 탄산수 1,200원 5개 탄산2+1",
+                "- 탄산수 1,200원 재고 없음",
+                "- 물 500원 10개",
+                "- 비타민워터 1,500원 6개",
+                "- 감자칩 1,500원 5개 반짝할인",
+                "- 감자칩 1,500원 5개",
+                "- 초코바 1,200원 5개 MD추천상품",
+                "- 초코바 1,200원 5개",
+                "- 에너지바 2,000원 5개",
+                "- 정식도시락 6,400원 8개",
+                "- 컵라면 1,700원 1개 MD추천상품",
+                "- 컵라면 1,700원 10개"
+        );
     }
 
     @Test
