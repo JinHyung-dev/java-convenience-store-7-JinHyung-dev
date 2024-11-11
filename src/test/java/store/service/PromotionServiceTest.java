5597921368a77e0b524c5c4af1202d5c76d3a838
JinHyung-Park <trupjh@gmail.com>
2024-11-11 16:46:41 +0900

    test(PromotionServiceTest) : 프로모션 조건 처리 테스트 작성 및 확인
    - 프로모션 buy 조건만큼 담았을 때 : 증정 안내 메세지 출력 & 사용자 입력 처리
       - Y : 프로모션 get 증정 개수 추가
       - N : 현재 수량 그대로 계속 진행
    - 프로모션 buy 조건보다 부족할 때 : 프로모션 없이 정가 결재 안내 메세지 출력 & 사용자 입력 처리
       - Y : 현재 수량만큼 정가 결재로 계속 진행
       - N : 해당 상품을 구매에서 제외

--- src/test/java/store/service/PromotionServiceTest.java
+++ src/test/java/store/service/PromotionServiceTest.java
@@ -1,8 +1,11 @@
 package store.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.ByteArrayInputStream;
 import org.junit.jupiter.api.Test;
+import store.domain.Cart;
 
 public class PromotionServiceTest {
     @Test
@@ -14,4 +17,51 @@ public class PromotionServiceTest {
                 "허용되지 않은 프로모션이 포함되었습니다: " + excludedPromotion);
     }
 
+    @Test
+    void 프로모션_조건충족시_추가() {
+        PurchaseService purchaseService = new PurchaseService();
+        purchaseService.makeShoppingCart(new String[]{"[콜라-2]"});
+
+        String input = "Y";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        PromotionService.getInstance().scanCartItemWithPromotion();
+        assertThat(Cart.getInstance().getFreeGet().size()).isEqualTo(1);
+    }
+
+    @Test
+    void 프로모션_조건충족시_추가거부() {
+        PurchaseService purchaseService = new PurchaseService();
+        purchaseService.makeShoppingCart(new String[]{"[콜라-2]"});
+
+        String input = "N";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        PromotionService.getInstance().scanCartItemWithPromotion();
+        assertThat(Cart.getInstance().getFreeGet().size()).isEqualTo(0);
+    }
+
+    @Test
+    void 프로모션_조건불충족시_정가구매_수락() {
+        PurchaseService purchaseService = new PurchaseService();
+        purchaseService.makeShoppingCart(new String[]{"[콜라-1]"});
+
+        String input = "Y";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        PromotionService.getInstance().scanCartItemWithPromotion();
+        assertThat(Cart.getInstance().getFreeGet().size()).isEqualTo(0);
+    }
+
+    @Test
+    void 프로모션_조건불충족시_정가구매_거부() {
+        PurchaseService purchaseService = new PurchaseService();
+        purchaseService.makeShoppingCart(new String[]{"[콜라-1]"});
+
+        String input = "N";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        PromotionService.getInstance().scanCartItemWithPromotion();
+        assertThat(Cart.getInstance().getCart().size()).isEqualTo(0);
+    }
 }
