143a52f7612c69249839a7b4194c5f9f103b1b2a
JinHyung-Park <trupjh@gmail.com>
2024-11-11 12:26:39 +0900

    refactor(InventoryServiceTest) : 카트 클래스 생성에 따른 입력 물품의 재고 확인 로직 변경

--- src/test/java/store/service/InventoryServiceTest.java
+++ src/test/java/store/service/InventoryServiceTest.java
@@ -1,25 +1,44 @@
 package store.service;
 
-public class InventoryServiceTest {
-    private final InventoryService inventoryService = new InventoryService();
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-//    @Test
-//    void 프로모션_없고_일반재고_없으면_에러() {
-//        Map<String, Integer> sampleCart = new HashMap<>();
-//        sampleCart.put("오렌지주스", 12);
-//        inventoryService.loadInventory(); //products 생성
-//
-//        assertThrows(IllegalArgumentException.class,
-//                () -> sampleCart.forEach((cart, quantity) -> inventoryService.checkInventoryStock(cart)));
-//    }
-//
-//    @Test
-//    void 일반제품_일반재고_없으면_에러() {
-//        Map<String, Integer> sampleCart = new HashMap<>();
-//        sampleCart.put("비타민워터", 7);
-//        inventoryService.loadInventory(); //products 생성
-//
-//        assertThrows(IllegalArgumentException.class,
-//                () -> sampleCart.forEach((cart, quantity) -> inventoryService.checkInventoryStock(cart)));
-//    }
+import java.io.ByteArrayInputStream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import store.domain.Cart;
+
+public class InventoryServiceTest {
+    private InventoryService inventoryService;
+    private PurchaseService purchaseService;
+
+    @BeforeEach
+    void 재고목록생성_및_카트생성() {
+        inventoryService = InventoryService.getInstance();
+        InventoryService.loadInventory(); //products 생성
+    }
+
+    @Test
+    void 프로모션_제품_일반재고_없으면_에러() {
+        purchaseService =  new PurchaseService();
+
+        String input = "[오렌지주스-12]";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        purchaseService.getItems();
+
+        assertThrows(IllegalArgumentException.class,
+                () -> inventoryService.checkInventoryStock(Cart.getInstance()));
+    }
+
+    @Test
+    void 일반제품_일반재고_없으면_에러() {
+        purchaseService =  new PurchaseService();
+
+        String input = "[비타민워터-7]";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        purchaseService.getItems();
+
+        assertThrows(IllegalArgumentException.class,
+                () -> inventoryService.checkInventoryStock(Cart.getInstance()));
+    }
+
 }
