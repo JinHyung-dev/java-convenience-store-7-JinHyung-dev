74709cf77c1ca1ee7197203a08f89adb8ca3b473
JinHyung-Park <trupjh@gmail.com>
2024-11-11 23:33:12 +0900

    feat(OutputView, ReceiptService) : 영수증 출력 로직 작성

---
+++ src/main/java/store/service/ReceiptService.java
@@ -0,0 +1,75 @@
+package store.service;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import store.domain.Cart;
+import store.domain.MemberShip;
+import store.domain.Product;
+import store.view.OutputView;
+
+public class ReceiptService {
+    private final OutputView outputView = new OutputView();
+    private final DecimalFormat decimalFormat = new DecimalFormat("#,##0");
+
+    public void makeReceipt(Map<Product, Integer> priceByProduct) {
+        String buyDetail = makeBuyDetail(priceByProduct);
+        outputView.printBuyDetail(buyDetail);
+        String giveDetail = calculateGiveDetail();
+        outputView.printGiveDetail(giveDetail);
+        int[] totalNumbers = calculateTotalDetail(priceByProduct);
+        String totalDetail = makeTotalDetail(totalNumbers);
+        outputView.printTotalAmount(totalDetail);
+    }
+
+    private String makeTotalDetail(int[] totalNumbers) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(String.format("%-12s\t%-8d%-10s\n", "총구매액", totalNumbers[0], decimalFormat.format(totalNumbers[1])))
+                .append(String.format("%-12s\t\t\t%-8s\n", "행사할인", "-" + decimalFormat.format(totalNumbers[2])))
+                .append(String.format("%-12s\t\t\t%-8s\n", "멤버십할인", "-" + decimalFormat.format(totalNumbers[3])))
+                .append(String.format("%-12s\t\t\t%-8s\n", "내실돈", decimalFormat.format(totalNumbers[4])));
+        return builder.toString();
+    }
+
+    private String makeBuyDetail(Map<Product, Integer> priceByProduct) {
+        Map<Product, Integer> cart = Cart.getInstance().getCart();
+        StringBuilder builder = new StringBuilder();
+        priceByProduct.forEach((product, price) -> {
+            builder.append(String.format("%-12s\t%-8d%-10s\n",
+                    product.getName(), cart.get(product), decimalFormat.format(price)));
+        });
+        return builder.toString();
+    }
+
+    private String calculateGiveDetail() {
+        Map<Product, Integer> give = Cart.getInstance().getFreeGet();
+        StringBuilder builder = new StringBuilder();
+        give.forEach((product, quantity) -> {
+            builder.append(String.format("%-12s\t\t\t%-8s\n",
+                    product.getName(), quantity));
+        });
+        return builder.toString();
+    }
+
+    private int[] calculateTotalDetail(Map<Product, Integer> priceByProduct) {
+        PurchaseService purchaseService = new PurchaseService();
+        int buyQuantity = Cart.getInstance().getCart().size();
+        int totalBuyAmount = purchaseService.getSum(priceByProduct);
+        int promotionDiscountAmount = calculatePromotionDiscount();
+        int memberDiscountAmount = (int) Math.round(MemberShip.SILVER.getDiscountAmount(totalBuyAmount));
+        int totalPayment = (int) Math.round(MemberShip.SILVER.applyDiscount(totalBuyAmount));
+        return new int[]{buyQuantity, totalBuyAmount, promotionDiscountAmount, memberDiscountAmount, totalPayment};
+    }
+
+    private int calculatePromotionDiscount() {
+        Map<Product, Integer> freeGet = Cart.getInstance().getFreeGet();
+        List<Integer> eachDiscount = new ArrayList<>();
+        freeGet.forEach(((product, quantity) -> {
+            eachDiscount.add(product.getPrice() * quantity);
+        }));
+        return eachDiscount.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+}
--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -1,6 +1,12 @@
 package store.view;
 
 public class OutputView {
+    private final String RECEIPT_HEADER = "===========W 편의점=============";
+//    private final String RECEIPT_DETAIL_HEADER = String.format("%-15s%-6s%-10s", "상품명", "수량", "금액");
+    private final String RECEIPT_DETAIL_HEADER = "상품명\t\t\t수량\t\t금액";
+    private final String RECEIPT_FREEGIVE_HEADER = "===========증\t정=============";
+    private final String RECEIPT_LINE = "==============================";
+
     public void helloCustomer() {
         System.out.println("안녕하세요. W편의점입니다.");
         System.out.println("현재 보유하고 있는 상품입니다.\n");
@@ -20,5 +26,25 @@ public class OutputView {
 
     public void printChoiceForMemberDiscount() {
         System.out.println("멤버십 할인을 받으시겠습니까? (Y/N)");
+    }
+
+    public void printBuyDetail(String detail) {
+        System.out.println(RECEIPT_HEADER);
+        System.out.println(RECEIPT_DETAIL_HEADER);
+        System.out.print(detail);
+    }
+
+    public void printGiveDetail(String detail) {
+        System.out.println(RECEIPT_FREEGIVE_HEADER);
+        System.out.print(detail);
+    }
+
+    public void printTotalAmount(String detail) {
+        System.out.println(RECEIPT_LINE);
+        System.out.println(detail);
+    }
+
+    public void printChoiceForOtherBuy() {
+        System.out.println("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)");
     }
 }
