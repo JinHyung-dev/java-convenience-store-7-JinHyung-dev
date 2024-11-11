10fd50da15fa79efb06155c72dd848251b29325f
JinHyung-Park <trupjh@gmail.com>
2024-11-11 16:18:27 +0900

    style(OutputView) : 증정제품 추가 출력문 띄어쓰기 수정

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -11,7 +11,7 @@ public class OutputView {
     }
 
     public void printChoiceForFreeGet(String productName) {
-        System.out.println("현재 " + productName +" 은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)");
+        System.out.println("현재 " + productName +"은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)");
     }
 
     public void printChoiceForNoPromotion(String name, int quantity) {
